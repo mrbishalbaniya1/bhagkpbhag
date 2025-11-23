8c3794b691724a6a1c61422c5ccb6ac51998eb82
Bishal Baniya <mrbishalbaniya1@gmail.com>
2025-11-23 16:50:44 +0000

    connect this to firebae // Import the functions you need from the SDKs y

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-336425836-be177",
-  "appId": "1:112166484548:web:3fb995c5d74ba252755a11",
-  "apiKey": "AIzaSyBLohVMBdUK80dXS3qiahK2iAgkr-zmwnk",
-  "authDomain": "studio-336425836-be177.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "112166484548"
+  apiKey: "AIzaSyBLohVMBdUK80dXS3qiahK2iAgkr-zmwnk",
+  authDomain: "studio-336425836-be177.firebaseapp.com",
+  projectId: "studio-336425836-be177",
+  storageBucket: "studio-336425836-be177.firebasestorage.app",
+  messagingSenderId: "112166484548",
+  appId: "1:112166484548:web:3fb995c5d74ba252755a11"
 };
